612785d407ca7968698eb546cebfd281e9bdbf78
sanadee_2004 <sanadeehaputhanthire@gmail.com>
2026-03-01 01:31:42 +0530

    add snake

--- snakeApp1.py
+++ snakeApp1.py
@@ -6,4 +6,17 @@ wn.bgcolor("skyblue")
 wn.setup(width=600, height=600)
 wn.tracer(0)
 
+
+
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+while True:
+    wn.update()
+
 wn.mainloop()
