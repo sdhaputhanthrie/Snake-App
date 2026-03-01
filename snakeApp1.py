279a6ad857ac78855e638c6e67a2fa9ac49d6029
sanadee_2004 <sanadeehaputhanthire@gmail.com>
2026-03-01 22:43:46 +0530

    body collisions

--- snakeApp1.py
+++ snakeApp1.py
@@ -28,20 +28,24 @@ food.color("blue")
 food.penup()
 food.goto(0,100)
 
-segments = [  ]
+segments = []
 
   
 def go_up():
-    head.direction = "up"
+    if head.direction != "down":
+        head.direction = "up"
 
 def go_down():
-    head.direction = "down"
+    if head.direction != "up":
+        head.direction = "down"
  
 def go_right():
-    head.direction = "right"
+    if head.direction != "left":
+        head.direction = "right"
 
 def go_left():
-    head.direction = "left"  
+    if head.direction != "right":
+        head.direction = "left"  
               
 def move():
     if head.direction == "up":
@@ -76,10 +80,9 @@ while True:
         head.goto(0,0)
         head.direction = "stop"
 
-    for segment in segments:
-        segment.goto(1000, 1000)
-
-    segments.clear()      
+        for segment in segments:
+            segment.hideturtle()
+        segments.clear()
 
     if head.distance(food) < 20:
         x = random.randint(-290,290)
@@ -104,6 +107,17 @@ while True:
          segments[0].goto(x,y)   
 
     move()
+
+    for segment in segments:
+        if segment.distance(head) < 20 :
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+
+            for segment in segments:
+                segment.hideturtle()
+
+            segments.clear()        
 
     time.sleep(delay)
 
