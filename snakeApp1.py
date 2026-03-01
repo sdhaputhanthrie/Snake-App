848649fc370c9886107ad7353e7c662de3468cc6
sanadee_2004 <sanadeehaputhanthire@gmail.com>
2026-03-01 17:39:22 +0530

    set border collisions

--- snakeApp1.py
+++ snakeApp1.py
@@ -71,6 +71,16 @@ wn.onkeypress(go_right, "d")
 while True:
     wn.update()
 
+    if head.xcor()>290 or head.xcor() <-290 or head.ycor()>290 or head.ycor()<-290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction = "stop"
+
+    for segment in segments:
+        segment.goto(1000, 1000)
+
+    segments.clear()      
+
     if head.distance(food) < 20:
         x = random.randint(-290,290)
         y = random.randint(-290,290)
